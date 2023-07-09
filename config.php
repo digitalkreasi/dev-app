73854f8279d25a38ac832af57f7b78e73327e6c0
digitalkreasi <admin@digitalkreasigroup.com>
2023-07-09 16:01:40 +0700

    UPP
    Co-Authored-By: rama <67516691+rama@users.noreply.github.com>

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+date_default_timezone_set('Asia/Jakarta');
+error_reporting(0);
+$maintenance = 0; // Maintenance? 1 = ya 0 = tidak
+if($maintenance == 1) {
+    die("site Maintance");
+}
+// database
+$config['db'] = array(
+	'host' => 'localhost',
+	'name' => 'indt5495_app',
+	'username' => 'indt5495_app',
+	'password' => 'Rama1234.'
+);
+
+mysqli_real_escape_string($conn = mysqli_connect($config['db']['host'], $config['db']['username'], $config['db']['password'], $config['db']['name']));
+if(!$conn) {
+	die("Koneksi Gagal : ".mysqli_connect_error());
+	}
+$config['web'] = array(
+	'url' => 'https://app.digitalkreasigroup.com/' // contoh: htdtp://sdmin.com/
+);
+// date & time
+$date = date("Y-m-d");
+$time = date("H:i:s");
+require("lib/function.php");
+require("lib/setting.php");
+?>
---
+++ lib/setting.php
@@ -0,0 +1,6 @@
+<?php
+//Pengaturan Website
+$config['web'] = array(
+	'url' => 'https://app.digitalkreasigroup.com/' // ex: http://domain.com/
+);
+?>
