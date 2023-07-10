f71fd368d484657f5d306875600b7fb1cf4fb9ca
digitalkreasi <137399378+digitalkreasi@users.noreply.github.com>
2023-07-10 23:32:53 +0700

    Update logout.php
    UNSET

--- logout.php
+++ logout.php
@@ -5,6 +5,7 @@ require("config.php");
 $insert_user = $conn->query("INSERT INTO aktifitas VALUES ('', '".$_SESSION['user']['username']."', 'Keluar', '".get_client_ip()."', '$date', '$time')");
 if ($insert_user == TRUE) {
 unset($_SESSION['user']);
+unset($_SESSION['username']);
 unset($_COOKIE['cookie_token']);
 setcookie("cookie_token",'');
 exit(header("Location: ".$config['web']['url']."auth/login"));
