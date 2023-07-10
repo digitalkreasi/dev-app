713d19b402aca7c30613584a5a753df247ae6465
Isra Yehova Asmaranda <138007389+israyehovax@users.noreply.github.com>
2023-07-10 15:18:39 +0700

    pergantian config
    pergantian config

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 date_default_timezone_set('Asia/Jakarta');
 error_reporting(0);
 $maintenance = 0; // Maintenance? 1 = ya 0 = tidak
-if($maintenance == 1) {
-    die("site Maintance");
+if ($maintenance == 1) {
+	die("site Maintance");
 }
 // database
 $config['db'] = array(
@@ -14,15 +14,14 @@ $config['db'] = array(
 );
 
 mysqli_real_escape_string($conn = mysqli_connect($config['db']['host'], $config['db']['username'], $config['db']['password'], $config['db']['name']));
-if(!$conn) {
-	die("Koneksi Gagal : ".mysqli_connect_error());
-	}
+if (!$conn) {
+	die("Koneksi Gagal : " . mysqli_connect_error());
+}
 $config['web'] = array(
-	'url' => 'https://app.digitalkreasigroup.com/' // contoh: htdtp://sdmin.com/
+	'url' => 'https://digitalkreasigroup.com/indofazz' // contoh: htdtp://sdmin.com/
 );
 // date & time
 $date = date("Y-m-d");
 $time = date("H:i:s");
 require("lib/function.php");
 require("lib/setting.php");
-?>
