bc787ecf22a56d04ea2d153f1ca37ffb4a6be548
Isra Yehova Asmaranda <138007389+israyehovax@users.noreply.github.com>
2023-07-13 13:31:09 +0700

    perbaikan csrf
    perbaikan csrf_token.php

--- lib/csrf_token.php
+++ lib/csrf_token.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Penulis Kode - SMM Panel script
  * Domain: http://penuliskode.com/
@@ -7,16 +8,16 @@
 
 if (empty($_SESSION['token'])) {
 	if (function_exists('mcrypt_create_iv')) {
-		$_SESSION['token'] = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
+		$_SESSION['token'] = bin2hex(random_bytes(32));
 	} else {
 		$_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(32));
 	}
 }
 $config['csrf_token'] = $_SESSION['token'];
 
-if ($_POST AND !isset($_POST['csrf_token'])) {
+if ($_POST and !isset($_POST['csrf_token'])) {
 	exit("Permintaan tidak diterima.");
-} elseif ($_POST AND isset($_POST['csrf_token'])) {
+} elseif ($_POST and isset($_POST['csrf_token'])) {
 	if (hash_equals($_SESSION['token'], $_POST['csrf_token']) == false) {
 		exit("Permintaan tidak diterima.");
 	}
