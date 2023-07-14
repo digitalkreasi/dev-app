ea3e38258727e8cef180686a2d9e202b1babab15
digitalkreasi <admin@digitalkreasigroup.com>
2023-07-14 16:36:05 +0700

    Update verifikasi_pin.php
    Co-Authored-By: rama <67516691+rama@users.noreply.github.com>

--- auth/verifikasi_pin.php
+++ auth/verifikasi_pin.php
@@ -35,106 +35,121 @@ if (isset($_SESSION['user'])) {
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Mobile-friendly Form</title>
-  <style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mobile-friendly Form</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <style>
     * {
-      box-sizing: border-box;
+        box-sizing: border-box;
     }
     
     body {
-      font-family: Arial, sans-serif;
-      margin: 0;
-      background-color: #f2f2f2;
+        font-family: 'Montserrat', sans-serif;
+        margin: 0;
+        background-color: #f2f2f2;
     }
     
     .container {
-      max-width: 400px;
-      width: 100%;
-      height: 100vh; /* Menggunakan unit vh untuk tinggi container */
-      margin: 0 auto;
-      background-color: #fff;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      position: relative;
+        max-width: 400px;
+        width: 100%;
+        height: 100vh; /* Menggunakan unit vh untuk tinggi container */
+        margin: 0 auto;
+        background-color: #fff;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        position: relative;
     }
     
-    h1 {
-      text-align: center;
+    h2 {
+        text-align: center;
+        position: relative;
+        top: -250px;
+        font-family: 'Montserrat', sans-serif;
+        font-weight: bolder;
     }
     
     p {
-      text-align: center;
+        text-align: center;
+        position: relative;
+        top: -230px;
+        
     }
     
     .logo {
-      text-align: center;
-      position: absolute;
-      top: 0;
-      left: 50%;
-      transform: translateX(-50%);
-      margin-top: 10px;
-      margin-bottom: 20px;
+        text-align: center;
+        position: absolute;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        margin-top: 10px;
+        margin-bottom: 20px;
     }
     
     .logo img {
-      max-width: 100%;
-      height: auto;
+        max-width: 100%;
+        height: auto;
     }
     
     form {
-      margin-top: 20px;
-      text-align: center;
+        margin-top: 20px;
+        text-align: center;
     }
     
     .pin-input-container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
     
     .pin-input-container input {
-      width: 30px;
-      height: 40px;
-      font-size: 24px;
-      text-align: center;
-      margin: 0 5px;
-      padding: 5px;
-      border: none;
-      border-bottom: 2px solid #ccc;
-      outline: none;
+        width: 30px;
+        height: 40px;
+        font-size: 24px;
+        text-align: center;
+        margin: 0 5px;
+        padding: 5px;
+        border: none;
+        border-bottom: 2px solid #ccc;
+        outline: none;
+        position: relative;
+        top: -190px;
     }
     
     .pin-input-container input:focus {
-      border-bottom-color: #4CAF50;
+        border-bottom-color: #4CAF50;
     }
-  </style>
+    a {
+        color: #00AFF0;
+        text-decoration: none;
+        position: relative;
+        bottom: -230px;
+        font-family: 'Montserrat', sans-serif;
+        font-weight: bold;
+    }
+    </style>
 </head>
 <body>
-  <div class="container">
-    <div class="logo">
-      <img src="indofazz.png" alt="Logo">
-    </div>
-    <form action="" method="POST" id="verifikasiPin" name="verifikasiPin">
-      <h1>Masukkan PIN</h1>
-      <p>Masukkan 6 digit PIN kamu</p>
-      <div class="pin-input-container">
+    <div class="container">
+        <form action="" method="POST" id="verifikasiPin" name="verifikasiPin">
+            <h2>Masukkan PIN</h2>
+            <p>Masukkan 6 digit PIN kamu</p>
+        <div class="pin-input-container">
         <input type="password" id="pin1" name="pin1" maxlength="1" oninput="handlePinInput(1)" onkeydown="handlePinKeyDown(event, 1)" required autofocus>
         <input type="password" id="pin2" name="pin2" maxlength="1" oninput="handlePinInput(2)" onkeydown="handlePinKeyDown(event, 2)" required>
         <input type="password" id="pin3" name="pin3" maxlength="1" oninput="handlePinInput(3)" onkeydown="handlePinKeyDown(event, 3)" required>
         <input type="password" id="pin4" name="pin4" maxlength="1" oninput="handlePinInput(4)" onkeydown="handlePinKeyDown(event, 4)" required>
         <input type="password" id="pin5" name="pin5" maxlength="1" oninput="handlePinInput(5)" onkeydown="handlePinKeyDown(event, 5)" required>
         <input type="password" id="pin6" name="pin6" maxlength="1" oninput="handlePinInput(6)" onkeydown="handlePinKeyDown(event, 6)" required>
-      </div>
-      <br>
-      <input type="submit" value="Submit" id="login" name="login" style="display: none;">
+        </div>
+        <br>
+        <input type="submit" value="Submit" id="login" name="login" style="display: none;">
     </form>
     <a href="reset-pin">Lupa PIN?</a>
-  </div>
-  <script>
+    </div>
+    <script>
     function handlePinInput(pinIndex) {
       const input = document.getElementById(`pin${pinIndex}`);
       const value = input.value;
